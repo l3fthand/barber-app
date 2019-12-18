6f71369fbf0a114a69772b57ba708114007f61f7
Taine Collins <taine.collins.1999@gmail.com>
2019-12-19 11:41:31 +1300

    local merge

---
+++ src/Landing.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Landing extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Landing;
